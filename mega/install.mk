4dda4cc10d8299e79a20dc7dd74f6d9e98a3a9cf
nobodyxu <Jiahao_XU@outlook.com>
2019-12-07 00:47:53 +1100

    Use of sudo instead of su_orig

--- mega/install.mk
+++ mega/install.mk
@@ -1,9 +1,9 @@
 install_desktop-env: de-install_mega
 
 de-add_mega_repo: install_curl
-	./desktop-env/mega/add_mega_repo.sh
-	$(apt) update
+	sudo ./desktop-env/mega/add_mega_repo.sh
+	sudo $(apt) update
 
 de-install_mega: de-add_mega_repo
-	$(apt) install -y megacmd
-	$(su_orig) ./desktop-env/mega/post_configure.sh
+	sudo $(apt) install -y megacmd
+	./desktop-env/mega/post_configure.sh
