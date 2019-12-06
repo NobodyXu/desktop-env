d30bc247732847602e9d29f821ad64c73d121866
nobodyxu <Jiahao_XU@outlook.com>
2019-12-06 23:59:09 +1100

    Apply the rule of variable name to target name.

--- mega/install.mk
+++ mega/install.mk
@@ -1,9 +1,9 @@
-install_desktop-env: desktop-env_install_mega
+install_desktop-env: de_install_mega
 
-desktop-env_add_mega_repo: install_curl
+de_add_mega_repo: install_curl
 	./desktop-env/mega/add_mega_repo.sh
 	$(apt) update
 
-desktop-env_install_mega: desktop-env_add_mega_repo
+de_install_mega: de_add_mega_repo
 	$(apt) install -y megacmd
 	$(su_orig) ./desktop-env/mega/post_configure.sh
