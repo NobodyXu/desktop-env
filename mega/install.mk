8dd23073814d70394998ffe28b5535d4d8923357
nobodyxu <Jiahao_XU@outlook.com>
2019-12-07 00:03:59 +1100

    Fix prefix ambiguity.

--- mega/install.mk
+++ mega/install.mk
@@ -1,9 +1,9 @@
-install_desktop-env: de_install_mega
+install_desktop-env: de-install_mega
 
-de_add_mega_repo: install_curl
+de-add_mega_repo: install_curl
 	./desktop-env/mega/add_mega_repo.sh
 	$(apt) update
 
-de_install_mega: de_add_mega_repo
+de-install_mega: de-add_mega_repo
 	$(apt) install -y megacmd
 	$(su_orig) ./desktop-env/mega/post_configure.sh
