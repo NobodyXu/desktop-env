32f84885bf98ebdd8a75006114e2721cc4275e6b
nobodyxu <Jiahao_XU@outlook.com>
2019-12-06 23:39:35 +1100

    Add dependency install_curl for add_mega_repo.
    Remove dependency install_curl for install_mega.

--- mega/install.mk
+++ mega/install.mk
@@ -1,9 +1,9 @@
 install_desktop-env: desktop-env_install_mega
 
-desktop-env_add_mega_repo:
+desktop-env_add_mega_repo: install_curl
 	./desktop-env/mega/add_mega_repo.sh
 	$(apt) update
 
-desktop-env_install_mega: install_curl desktop-env_add_mega_repo
+desktop-env_install_mega: desktop-env_add_mega_repo
 	$(apt) install -y megacmd
 	$(su_orig) ./desktop-env/mega/post_configure.sh
