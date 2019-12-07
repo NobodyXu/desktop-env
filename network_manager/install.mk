831843692541e4b6c469ab5dde8138325bf2288e
nobodyxu <Jiahao_XU@outlook.com>
2019-12-07 20:41:57 +1100

    Enable and start service after install

--- network_manager/install.mk
+++ network_manager/install.mk
@@ -3,3 +3,5 @@ install_desktop-env: de-install_network_manager
 de-install_network_manager: de-install_i3
 	sudo $(apt) install -y network-manager
 	$(de-add_user_unit) ./desktop-env/network_manager/nm-applet.service
+	systemctl --user enable nm-applet
+	systemctl --user start nm-applet
