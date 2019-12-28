ef2423122e60c13f282bfb1ecb86e6e380ae6886
Jiahao XU <Jiahao_XU@outlook.com>
2019-12-28 02:10:01 -0800

    Allow current user to use cups

--- cups/install.mk
+++ cups/install.mk
@@ -2,5 +2,9 @@ install_desktop-env: de-install_cups
 
 de-install_cups:
 	sudo $(apt) install -y cups
+				-e "s/Require user @SYSTEM"
+	sudo sed -i -e "s/Require user @SYSTEM/Require user @SYSTEM ${USER}/g" \
+				-e "s/Require user @OWNER @SYSTEM/Require user @OWNER @SYSTEM ${USER}/g" \
+				/etc/cups/cupsd.conf
 	sudo systemctl enable cups
 	./desktop-env/cups/post_configure.sh
