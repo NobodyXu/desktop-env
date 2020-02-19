fdd46689e3df4d07f32c0fe2a44c4e5ffa44b17c
nobodyxu <Jiahao_XU@outlook.com>
2020-02-19 14:48:27 +1100

    Use i3-battery-popup in i3

--- i3/install.mk
+++ i3/install.mk
@@ -5,6 +5,6 @@ de-i3_add_repo: install_apt-add-repository
 	./desktop-env/i3/add_i3_repo.sh
 	sudo $(apt) update
 
-de-install_i3: de-i3_add_repo de-install_feh de-install_amixer de-install_xbacklight
+de-install_i3: de-i3_add_repo de-install_feh de-install_amixer de-install_xbacklight de-install_i3-battery-popup
 	sudo $(apt) install -y i3 i3lock-fancy i3blocks bash imagemagick
 	./desktop-env/i3/post_configure.sh
