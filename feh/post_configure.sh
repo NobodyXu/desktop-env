377bdecde22e9166cdd0994c03e85ec2d67c1f1e
nobodyxu <Jiahao_XU@outlook.com>
2020-03-24 22:23:43 +1100

    Fix feh/post_configure.sh

--- feh/post_configure.sh
+++ feh/post_configure.sh
@@ -4,6 +4,9 @@ wallpapers=~/Pictures/Wallpapers/
 
 mkdir -p $wallpapers
 
+# Add systemd timer that automatically changes background
+cp ./desktop-env/feh/update-desktop-background.* ~/.config/systemd/user/
+
 cp ./desktop-env/feh/space.jpg $wallpapers/
 
 # Symlink system-provided backgrounds into wallpapers
@@ -11,6 +14,3 @@ cd $wallpapers
 for pic in /usr/share/backgrounds/*/*; do
     ln -s $pic
 done
-
-# Add systemd timer that automatically changes background
-cp ./desktop-env/feh/update-desktop-background.* ~/.config/systemd/user/
